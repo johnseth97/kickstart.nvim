cc090a6e57a44ba5cba4a6adaea7e5fdabbabd29
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2024-10-16 21:30:28 -0400

    broke-things-apart

--- lua/custom/plugins/nvim-hilight-colors.lua
+++ lua/custom/plugins/nvim-hilight-colors.lua
@@ -1,10 +1,6 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {
+-- Highlight colors in your code
 
-  -- Color Picker
+return {
   {
     'brenoprata10/nvim-highlight-colors',
     config = function()
@@ -63,47 +59,5 @@ return {
         exclude_buftypes = {},
       }
     end,
-  },
-  { -- Dashboard
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        theme = 'hyper',
-        config = {
-          week_header = {
-            enable = true,
-          },
-          shortcut = {
-            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-            {
-              icon = ' ',
-              icon_hl = '@variable',
-              desc = 'Files',
-              group = 'Label',
-              action = 'Telescope find_files',
-              key = 'f',
-            },
-            {
-              desc = ' Apps',
-              group = 'DiagnosticHint',
-              action = 'Telescope app',
-              key = 'a',
-            },
-            {
-              desc = ' dotfiles',
-              group = 'Number',
-              action = 'Telescope dotfiles',
-              key = 'd',
-            },
-          },
-        },
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-  },
-
-  { -- Github Copilot
-    'github/copilot.vim',
   },
 }
