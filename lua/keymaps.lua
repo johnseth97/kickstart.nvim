4198a98036ed2e266f25502bffdf1e611042caa5
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2024-12-03 23:44:16 -0500

    Made changes to the state of things

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,17 +13,33 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 --
+-- Remap jkl; to hjkl in normal mode
+--  This is a personal preference, but I find it easier to use the home row keys
+-- Map 'j' to move left while supporting motions like '3j'
+vim.keymap.set('n', 'j', 'h', { noremap = true, desc = 'Move cursor left' })
+
+-- Map 'k' to move down while supporting motions like '3k'
+vim.keymap.set('n', 'k', 'j', { noremap = true, desc = 'Move cursor down' })
+
+-- Map 'l' to move up while supporting motions like '3l'
+vim.keymap.set('n', 'l', 'k', { noremap = true, desc = 'Move cursor up' })
+
+-- Map ';' to move right while supporting motions like '3;'
+vim.keymap.set('n', ';', 'l', { noremap = true, desc = 'Move cursor right' })
+
+--
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use ; to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use k to move!!"<CR>')
 --
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Remap split navigation
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, desc = 'Move focus to the left split' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, desc = 'Move focus to the bottom split' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, desc = 'Move focus to the top split' })
+vim.keymap.set('n', '<C-;>', '<C-w>;', { noremap = true, desc = 'Move focus to the right split' })
