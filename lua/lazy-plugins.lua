343199138bbd03f20c5d7a9a4f929efc013e6020
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-03-20 16:23:47 -0400

    fixing lua plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -337,12 +337,12 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = { 'lua_ls', 'pyright', 'tsserver', 'gopls', 'rust_analyzer' }, -- List of LSPs you want installed
+        automatic_installation = true, -- Automatically install missing LSPs
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            -- Ensure capabilities are merged correctly
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
@@ -357,7 +357,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo', 'Format' },
     keys = {
       {
-        '<leader>f',
+        '<leader>F',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
