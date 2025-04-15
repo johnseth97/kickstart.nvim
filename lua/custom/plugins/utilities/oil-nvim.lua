57633b97f453a161159810594c993ff3f81fb048
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-04-15 11:05:47 -0500

    fixed issue with tresitter not loading properly on start

--- lua/custom/plugins/utilities/oil-nvim.lua
+++ lua/custom/plugins/utilities/oil-nvim.lua
@@ -7,7 +7,6 @@ return {
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
   -- Map <leader>f to open the file browser
   vim.keymap.set('n', '<leader>f', function()
     require('oil').open_float()
