48b64e9586f9cc69b5439646a6e4faa8f5110eaa
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-04-15 11:28:30 -0500

    updated nvim-dadbod to automatically load db connections
    place db connections in .env.local, within the root of your specific
    projects
    
    envs are formatted as `DATABASE_<name>`, i.e. `DATABASE_TEST_DB` will be
    parsed into dadbod as `TEST DB`
    
    The value of the env is the connection string, formatted as
    dbtype://user:pass@address?options

--- lua/custom/plugins/utilities/nvim-dadbod-ui.lua
+++ lua/custom/plugins/utilities/nvim-dadbod-ui.lua
@@ -1,8 +1,8 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    'tpope/vim-dadbod',
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' } },
   },
   cmd = {
     'DBUI',
@@ -10,8 +10,42 @@ return {
     'DBUIAddConnection',
     'DBUIFindBuffer',
   },
-  init = function()
-    -- Your DBUI configuration
+  event = 'VeryLazy',
+  config = function()
+    vim.g.db_ui_dotenv_variable_prefix = '__DISABLED__'
+    local handle = io.popen 'printenv'
+    local result = handle:read '*a'
+    handle:close()
+
+    local connections = {}
+
+    for line in result:gmatch '[^\r\n]+' do
+      local key, value = line:match '^(.-)=(.*)$'
+      if key and key:match '^DATABASE_' then
+        local suffix = key:gsub('^DATABASE_', '')
+        local name = suffix:gsub('_', ' '):upper()
+
+        -- Extract database name from URL for path fallback
+        local db_path = value:match '.*/(.*)'
+        db_path = db_path or name
+
+        table.insert(connections, {
+          name = name,
+          url = value,
+          path = 'default', -- stub for UI schema tree
+        })
+
+        print('[DBUI] Added connection: ' .. name)
+      end
+    end
+
+    if #connections > 0 then
+      vim.g.dbs = connections
+      print('[DBUI] ' .. #connections .. ' connections registered.')
+    else
+      print '[DBUI] No DATABASE_* env vars found.'
+    end
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = vim.fn.stdpath 'data' .. '/db_ui'
   end,
 }
