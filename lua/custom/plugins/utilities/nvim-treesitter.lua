8203575218f41c80487233be51fe4060e4d80420
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-05-21 13:33:50 -0500

    cleaned up treesitter

--- lua/custom/plugins/utilities/nvim-treesitter.lua
+++ lua/custom/plugins/utilities/nvim-treesitter.lua
@@ -1,26 +1,48 @@
-return { -- Treesitter: Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  event = 'BufReadPost',
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    event = 'BufReadPost',
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby', 'lua' },
+        disable = function(_, bufnr)
+          return vim.wo[vim.api.nvim_get_current_win()].diff
+        end,
+      },
+      indent = { enable = true, disable = { 'ruby' } },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    config = function(_, opts)
+      -- 1) Standard TS setup:
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- 2) Whenever any window-option 'diff' changes, stop TS in that buffer:
+      vim.api.nvim_create_autocmd('OptionSet', {
+        pattern = 'diff',
+        callback = function(args)
+          local bufnr = args.buf
+          if vim.api.nvim_buf_get_option(bufnr, 'filetype') ~= '' and vim.wo[args.win].diff then
+            -- stop the highlighter for this buffer
+            require('vim.treesitter.highlighter').stop(bufnr)
+          end
+        end,
+      })
+    end,
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
